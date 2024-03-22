13bb2955584f61b2453242877d879d0d00fe19f0
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2024-03-22 07:15:20 -0400

    changes

--- 01-Birthday-Reminder/src/app/app.component.ts
+++ 01-Birthday-Reminder/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import data from '../data';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Birthday-Reminder';
+  people = data;
+
+  handleButtonClick() {
+    this.people = [];
+  }
 }
