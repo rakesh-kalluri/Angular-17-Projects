fd01e2fcbeb294bc088f3bcb95482e063adcc4f3
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2024-06-13 06:59:01 -0400

    added styling and html elements

--- 02-Quote-Generator/src/app/app.component.ts
+++ 02-Quote-Generator/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuoteViewComponent } from './quote-view/quote-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [QuoteViewComponent],
 })
 export class AppComponent {
   title = '02-Quote-Generator';
---
+++ 02-Quote-Generator/src/app/quote-view/quote-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './quote-view.component.html',
+  styleUrl: './quote-view.component.css'
+})
+export class QuoteViewComponent {
+
+}
