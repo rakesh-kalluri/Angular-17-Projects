fae5bce51ef6760e89353d5cbb45bff990b0d9c2
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2024-06-15 09:32:08 -0400

    implemented infinite scroll

--- 03-Infinite-Scroll/src/app/image-gallery/image-gallery.component.ts
+++ 03-Infinite-Scroll/src/app/image-gallery/image-gallery.component.ts
@@ -1,21 +1,56 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, HostListener, OnInit, inject } from '@angular/core';
 import { ImageGalleryService } from '../image-gallery.service';
 
 @Component({
   selector: 'app-image-gallery',
   standalone: true,
-  imports: [],
   templateUrl: './image-gallery.component.html',
-  styleUrl: './image-gallery.component.css',
+  styleUrls: ['./image-gallery.component.css'],
 })
 export class ImageGalleryComponent implements OnInit {
-  photos: any = [];
+  photos: any[] = [];
   private imageGalleryService = inject(ImageGalleryService);
+  ready = false;
+  imagesLoaded = 0;
+  totalImages = 0;
 
   ngOnInit(): void {
-    this.imageGalleryService.getPhotosFromApi().subscribe((photos: any) => {
-      this.photos = photos;
-      console.log(this.photos);
-    });
+    this.getPhotos();
+  }
+
+  getPhotos() {
+    this.ready = false;
+    this.imageGalleryService.getPhotosFromApi().subscribe(
+      (photos: any) => {
+        this.photos = [...this.photos, ...photos]; // Append new photos to the existing array
+        this.totalImages = this.photos.length;
+        this.imagesLoaded = 0; // Reset the count of loaded images
+        console.log('New images loaded, total images:', this.totalImages);
+      },
+      (error) => {
+        console.error('Error fetching photos:', error);
+      }
+    );
+  }
+
+  imageLoaded() {
+    this.imagesLoaded++;
+    console.log(`Image loaded, current count: ${this.imagesLoaded}`);
+    if (this.imagesLoaded === this.totalImages) {
+      this.ready = true;
+      console.log('All images loaded, setting ready to true');
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    if (
+      window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 1000 &&
+      this.ready
+    ) {
+      console.log('Loading new images');
+      this.getPhotos();
+    }
   }
 }
