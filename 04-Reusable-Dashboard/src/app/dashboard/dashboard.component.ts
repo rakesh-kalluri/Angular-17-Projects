f540dee38dcb95767926aa482f1cd43a9ed0343d
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2024-07-26 07:45:54 -0400

    Add semantic HTML skeleton for dashboard, dashboard-item, and form-control components

--- 04-Reusable-Dashboard/src/app/dashboard/dashboard.component.ts
+++ 04-Reusable-Dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { FormControlComponent } from '../form-control/form-control.component';
+import { DashboardItemComponent } from '../dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FormControlComponent, DashboardItemComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
