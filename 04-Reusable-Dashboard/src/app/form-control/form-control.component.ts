e2f51597e19162555428cc3d5e0c458fad2dc6e5
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2024-07-26 07:31:16 -0400

    Generate initial components: dashboard, dashboard-item, and form-control

---
+++ 04-Reusable-Dashboard/src/app/form-control/form-control.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-control.component.html',
+  styleUrl: './form-control.component.css'
+})
+export class FormControlComponent {
+
+}
