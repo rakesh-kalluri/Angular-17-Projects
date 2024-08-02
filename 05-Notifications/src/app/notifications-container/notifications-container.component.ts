be256f912cb6fc9e8e5313f89fd69c7f4eb8d5fb
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2024-08-02 06:33:30 -0400

    feature:Set up Angular project and create initial components
    Initialize new Angular project 'notifications-app'
    
    Generate 'notifications-container' component
    
    Generate 'notification' component
    
    Set up basic project structure and configurations

---
+++ 05-Notifications/src/app/notifications-container/notifications-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './notifications-container.component.html',
+  styleUrl: './notifications-container.component.css'
+})
+export class NotificationsContainerComponent {
+
+}
