1d5e20e4a93a15e8f6a2b3557188f0c7fac64c24
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2024-08-08 15:02:02 -0400

    base tasks structure

--- 06-Todo-App/src/app/todo-list/todo-list.component.ts
+++ 06-Todo-App/src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.css'
+  styleUrl: './todo-list.component.css',
 })
 export class TodoListComponent {
+  private tasksService = inject(TaskService);
 
+  tasks = computed(() => this.tasksService.getAllTasks);
+
+  enteredTitle = '';
+  enteredTaskDueDate = '';
+
+  addTask() {
+    console.log(this.tasks());
+  }
 }
